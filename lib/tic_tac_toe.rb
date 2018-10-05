4fcaa2458a555a3c6661e4863dcc7d354b5a2c50
David masterson <david73180@gmail.com>
2018-10-05 23:03:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS =[
 
 
 
-def display_board(board)
+def display_board
 puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
 puts          "-----------"
 puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
